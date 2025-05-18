24dc4bcc8bb986575ba89c24555853d719ac5552
Eduardo <eduaedodiaz@gmail.com>
2025-05-18 13:47:28 -0600

    jobPage updated

--- client/src/pages/JobPage.jsx
+++ client/src/pages/JobPage.jsx
@@ -1,12 +1,17 @@
-import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Link, useParams } from 'react-router-dom';
 import { formatDate } from '../lib/formatters';
-import { jobs } from '../lib/fake-data';
+import {getJob} from "../lib/graphql/queries";
 
 function JobPage() {
   const { jobId } = useParams();
-
-  const job = jobs.find((job) => job.id === jobId);
+  const [job, setJob] = useState();
+  useEffect(() => {
+    getJob(jobId).then(setJob);
+  }, [jobId]);
+  if(!job){
+    return <div>Loading...</div>
+  }
   return (
     <div>
       <h1 className="title is-2">
